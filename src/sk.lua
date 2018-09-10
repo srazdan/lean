bcaf3e8c326a80127fce57b8f106afb95297bc78
Tim Menzies <tim.menzies@gmail.com>
2018-09-10 00:16:43 -0400

    pushing

--- src/sk.lua
+++ src/sk.lua
@@ -7,64 +7,40 @@ require "sample"
 require "stats"
 require "xtiles"
 
-
-before, mu     =  0, all.mu
-  for i,l,r in leftRight(parts,epsilon):
-    if big(l.n) and big(r.n):
-      n   = all.n * 1.0
-      now = l.n/n*(mu- l.mu)**2 + r.n/n*(mu- r.mu)**2
-      if now > before:
-        before,cut,left,right = now,i,l,r
-  return cut,left,right
-
-
 function sk(samples)
-  samples = sorted(samples, sampleLt)
+  local function inc(n,i,f)
+    f = f or numInc
+    for _,x in paris(samples[i]) do f(n,x) end end
 
-  local function add(n,i)
-    for _,x in paris(samples[i]) do numInc(n,x) end end
-  local function dec(n,i)
-    for _,x in paris(samples[i]) do numDec(n,x) end end
-  local function muXpect(all,l,r,   n)  
+  local function xpect(all,l,r,   n)  
     return l.n/all.n * (all.mu - l.mu)^2 + 
            r.n/all.n * (all.mu - r.mu)^2 end
 
-  local function argmax(c,lo,hi,     all,l,r,cut,best,tmp)
+  local function argmax(lo,hi,       all,l,r,cut,best,tmp)
     all, l, r = num(), num(), num()
-    for i=lo,hi do add(r,i); add(all,i) end
+    for i=lo,hi do inc(r,i); inc(all,i) end
     best = 0
     for i=lo,hi-1 do
-      add(l, i)
-      dec(r, i)
-      tmp = muXpect(all,l,r) * Lean.unsuper.margin
+      inc(l, i)
+      inc(r, i, numDec)
+      tmp = xpect(all,l,r) * Lean.unsuper.margin
       if tmp > best then
         cut,best = i, tmp end end 
-    return cut
-  end
+    return cut end
 
-  local function cuts(c,lo,hi,rank, pre,       cut,txt,b)
-    txt = pre..samples[c].mu..".."..samples[c].mu)
-    cut = argmax(c,lo,hi)
+  local function cuts(lo,hi,rank, pre,            cut,txt)
+    txt= pre .. nth(samples[lo],.5) .. nth(samples[hi],.5)
+    cut= argmax(lo,hi)
     if cut then
       fyi(txt)
-      rank = cuts(c,lo,    cut, rank, pre.."|.. ")+1
-      rank = cuts(c,cut+1, hi,  rank, pre.."|.. ")
+      rank = cuts(lo,    cut, rank, pre.."|.. ") + 1
+      rank = cuts(cut+1, hi,  rank, pre.."|.. ")
     else
-      fyi(txt.." ("..b..")")
-      for r=lo,hi do
-        samples[c] = rank end end
-    return rank
-  end
+      fyi(txt.." ("..rank..")")
+      for i=lo,hi do
+        samples[i] = rank end end
+    return rank end
 
-  for _,c  in pairs(data.indeps) do
-    if data.nums[c] then
-      if c==3 then
-        ksort(c,rows)
-        most = stop(c,rows)
-        fyi("\n-- ".. data.name[c] .. most .. "----------")
-        cuts(c,1,most,1, "|.. ") end end end
-  print(gsub( cat(data.name,", "), "%$","")) 
-  dump(rows)
+  samples = sorted(samples, sampleLt)
+  cuts(1, #samples,1,"|.. ")
 end
-
-return {main=function() return unsuper(rows()) end}
