23a90ad1dce7b0f6695a3659968910ee314102f7
Tim Menzies <tim.menzies@gmail.com>
2018-09-20 05:41:47 -0400

    pushing

--- src/lib.lua
+++ src/lib.lua
@@ -1,5 +1,5 @@
 -- vim: ts=2 sw=2 sts=2 expandtab:cindent:formatoptions+=cro   
---------- --------- --------- --------- --------- ---------~  
+--------- --------- --------- --------- --------- ---------    
 
 require "config"
 require "random"
