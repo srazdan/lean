29e81d02b3fb668eee20f40e1379366d7a57560e
Tim Menzies <tim.menzies@gmail.com>
2018-09-29 01:33:59 -0400

    as

--- src/tests.lua
+++ src/tests.lua
@@ -8,5 +8,8 @@ package.path = '../src/?.lua;' .. package.path
 
 for i,f in pairs(arg) do if i > 1 then 
   dofile(f) 
+  print("\n\n\n###### "..f.." ################")
+  print("#\n#\n#\tpass= "..Lean.ok.tries.. " fails= " .. Lean.ok.fails)
+  print("#\n#\n#################################")
 end end
-os.exit(Lean.ok.fails < 2 and 1 or 0)
+os.exit(Lean.ok.fails < 2 and 0 or 1)
